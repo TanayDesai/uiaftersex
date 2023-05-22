b2e13f857a6b566f0cde1c62ba1fc0b862219245
TanayDesai <tanaydesai5.com>
2023-05-23 03:13:56 +0530

    VD0.0.1

--- app/page.js
+++ app/page.js
@@ -1,95 +1,132 @@
+"use client"
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from '../styles/page.module.css'
+import React, {useState, useRef, useEffect} from 'react'
+import {ImCommand} from 'react-icons/im';
+import {HiArrowSmallRight} from "react-icons/hi2";
+import myGif from '../assests/1.gif'
+import myGif2 from '../assests/2.gif'
+import myGif3 from '../assests/3.gif'
+import {motion} from "framer-motion";
+
 
 export default function Home() {
+  const targetRef = useRef(null);
+
+        // const scrollToTarget = () => {
+        //   if (targetRef.current) {
+        //     targetRef.current.scrollIntoView({ behavior: 'smooth' });
+        //   }
+        // }
+
+  const scrollToTarget = () => {
+    const element = targetRef.current;
+    if (element) {
+      // const { top, left, height, width } = element.getBoundingClientRect();
+      // const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      // const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+      // const offsetTop = top + scrollTop - (window.innerHeight - height) / 2;
+      // const offsetLeft = left + scrollLeft - (window.innerWidth - width) / 2;
+      const { top, left, height, width } = element.getBoundingClientRect();
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+      const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+      const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+      const offsetTop = top + scrollTop - (viewportHeight - height) / 2;
+      const offsetLeft = left + scrollLeft - (viewportWidth - width) / 2;
+
+
+      window.scrollTo({
+        top: offsetTop,
+        left: offsetLeft,
+        behavior: 'smooth',})
+    }
+  }
+ 
+  useEffect(() => {
+      scrollToTarget()
+      const down = (e) => {
+        if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+          e.preventDefault()
+          scrollToTarget()
+          console.log("Done")
+        }
+      }
+      document.addEventListener('keydown', down)
+      return () => document.removeEventListener('keydown', down)
+    }, [])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div ref={targetRef} className={styles.main}>
+     <motion.div drag dragMomentum={false} style={{backgroundColor:"white",x:200,y:100}} whileHover={{scale:1.009, transition:{duration:0.2}}}  className={styles.display}>
+        <div className={styles.displayBar}>
+          <div className={styles.displayBarIcons}>
+          <div className={styles.displayBarIcon}></div>
+          <div className={styles.displayBarIcon}></div>
+          <div className={styles.displayBarIcon}></div>
         </div>
-      </div>
+        <div className={styles.displayBarSearchBox}>Site Search + ⌘</div>
+          <motion.div whileHover={{scale: 1.1}} className={styles.displayBarArrow}><HiArrowSmallRight color='black' size={20}></HiArrowSmallRight></motion.div>
+        </div>
+        <Image style={{display:"flex",margin:'auto',pointerEvents:'none'}} src={myGif} width={300} height={250} alt=""/>
+      </motion.div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <motion.div drag dragMomentum={false} style={{x:300,y:200,background: "#C37158",background: "radial-gradient(at center bottom, #D08C85, #67E8CF)"}} initial={{scale:0.7}} whileHover={{scale:0.709, transition:{duration:0.2}}} className={styles.display}>
+        <div className={styles.displayBar}>
+          <div className={styles.displayBarIcons}>
+          <div className={styles.displayBarIcon}></div>
+          <div className={styles.displayBarIcon}></div>
+          <div className={styles.displayBarIcon}></div>
+        </div>
+        <div className={styles.displayBarSearchBox}>Link Previews</div>
+          <motion.div whileHover={{scale: 1.1}} className={styles.displayBarArrow}><HiArrowSmallRight color='black' size={20}></HiArrowSmallRight></motion.div>
+        </div>
+        
+         {/* <Image style={{display:"flex",margin:'auto',pointerEvents:'none'}} width={300} height={200} alt=""/> */}
+      </motion.div> 
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <motion.div drag dragMomentum={false} style={{background:"radial-gradient(at center, rgba(215, 90, 88, 1.0), rgba(62, 37, 170, 1.0))", y:50,x:1240}} whileHover={{scale:1.009, transition:{duration:0.2}}}  className={styles.display}>
+        <div className={styles.displayBar}>
+          <div className={styles.displayBarIcons}>
+            <div className={styles.displayBarIcon}></div>
+            <div className={styles.displayBarIcon}></div>
+            <div className={styles.displayBarIcon}></div>
+          </div>
+          <div className={styles.displayBarSearchBox}>Peek</div>
+          <motion.div whileHover={{scale: 1.1}} className={styles.displayBarArrow}><HiArrowSmallRight color='black' size={20}></HiArrowSmallRight></motion.div>
+        </div>
+        {/* <Image style={{display:"flex",margin:'auto',pointerEvents:'none'}}  width={300} height={250} alt=""/> */}
+      </motion.div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+      <motion.div drag dragMomentum={false} style={{x:1300,y:-800}} className={styles.textBox} whileHover={{scale:1.008, transition:{duration:0.2}}}>
+        <div className={styles.textBoxBar}>
+          <div className={styles.displayBarIcons}>
+            <div className={styles.displayBarIcon}></div>
+            <div className={styles.displayBarIcon}></div>
+            <div className={styles.displayBarIcon}></div>
+          </div>
+          <div className={styles.displayBarSearchBox}>Notes</div>
+          <motion.div whileHover={{scale: 1.1}} className={styles.displayBarArrow}><HiArrowSmallRight color='black' size={20}></HiArrowSmallRight></motion.div>
+        </div>
+        <div className={styles.textBoxBottom}></div>
+      </motion.div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+      <motion.div style={{y:-1000}} className={styles.logoBox}>
+        <div className={styles.header}>
+            <h7>uiaftersex</h7>
+            <div><ImCommand size={25}></ImCommand></div>
+        </div>
+        <div className={styles.description}>
+          <p>A simple, modern, and accessible UI framework for Next.js.A simple, modern, and accessible UI framework. </p>
+          <p>A simple, modern, and <span>accessible UI framework for Next.js A simple</span> modern, and accessible UI framework for Next.js. </p>
+        </div> 
+        <div className={styles.footer}>
+          {/* <p>Built by <span>Tanay</span>✌</p> */}
+          <p><span>*signature*</span></p>
+        </div>
+      </motion.div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+    </div>
   )
 }
